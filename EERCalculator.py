c020673f2ab5fd7d366201920a786050163303e4
Fuong <fuongbregas@icloud.com>
2020-02-18 11:42:31 -0800

    backup

--- EERCalculator.py
+++ EERCalculator.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'ROC curve 1.xlsx')
 
-df1 = pd.read_csv("ROC curve 1.xlsx")
+excel1 = pd.ExcelFile(my_file)
+#excel1.sheet_names
+df1 = excel1.parse()
 df1.plot()
-df1.show()
+plt.show()
+#print(df1)
